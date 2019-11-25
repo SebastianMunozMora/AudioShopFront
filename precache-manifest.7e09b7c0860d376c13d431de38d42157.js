c51a13b26b6aba399ef3fa1e05751b599127c643
seb <smm9308@gmail.com>
2019-11-25 07:39:05 -0500

    Updates

---
+++ precache-manifest.7e09b7c0860d376c13d431de38d42157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793d12952306e34a812fd8171a82e007",
+    "url": "/AudioShopFront/index.html"
+  },
+  {
+    "revision": "7a66e70246d436a6b5ae",
+    "url": "/AudioShopFront/static/css/main.67610f05.chunk.css"
+  },
+  {
+    "revision": "6ee288a7a03b92e1b716",
+    "url": "/AudioShopFront/static/js/2.fec16120.chunk.js"
+  },
+  {
+    "revision": "7a66e70246d436a6b5ae",
+    "url": "/AudioShopFront/static/js/main.4c0c10f2.chunk.js"
+  },
+  {
+    "revision": "ac4e16c79d9205c006f9",
+    "url": "/AudioShopFront/static/js/runtime-main.9e9af7ae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/AudioShopFront/static/media/logo.25bf045c.svg"
+  }
+]);
